cf90bdda46a463f93aad7ae5c57017aa781b7e44
Micah Smith <micahadamsmith@gmail.com>
2020-09-11 09:33:25 -0400

    disable raw mode

--- vim-clone/src/main.rs
+++ vim-clone/src/main.rs
@@ -2,18 +2,42 @@ use std::io::{self};
 use std::os::unix::io::RawFd;
 use termios::*;
 
-fn enable_raw_mode(fd: RawFd) -> io::Result<()> {
-    let mut termios = Termios::from_fd(fd).unwrap();
-    termios.c_lflag = !(ECHO);
-    return tcsetattr(fd, TCSAFLUSH, &termios);
+#[derive(Debug)]
+struct Terminal {
+    fd: RawFd,
+    initial: Termios,
+    mutable: Termios,
+}
+
+impl Terminal {
+    fn new() -> Terminal {
+        let fd: RawFd = 0;
+        let termios = Termios::from_fd(fd).unwrap();
+
+        return Terminal {
+            fd: fd,
+            initial: termios,
+            mutable: termios.clone(),
+        };
+    }
+
+    fn enable_raw_mode(&mut self) {
+        let mut termios = self.mutable;
+        termios.c_lflag &= !(ECHO);
+        tcsetattr(self.fd, TCSAFLUSH, &termios).unwrap();
+    }
+
+    fn disable_raw_mode(&mut self) {
+        let termios = self.initial;
+        tcsetattr(self.fd, TCSAFLUSH, &termios).unwrap();
+    }
 }
 
 fn main() -> io::Result<()> {
+    let mut terminal = Terminal::new();
+    terminal.enable_raw_mode();
+
     let mut buffer = String::new();
-    let fd: RawFd = 0;
-
-    enable_raw_mode(fd).unwrap();
-
     loop {
         match io::stdin().read_line(&mut buffer) {
             Ok(n) => {
@@ -29,5 +53,6 @@ fn main() -> io::Result<()> {
         buffer.clear();
     }
 
+    terminal.disable_raw_mode();
     Ok(())
 }
