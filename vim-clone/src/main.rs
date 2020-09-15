8a69f33fcf09485c57cd64256ef728193d0fdb11
Micah Smith <micahadamsmith@gmail.com>
2020-09-15 16:18:10 -0400

    add const ctrl-q for exit

--- vim-clone/src/main.rs
+++ vim-clone/src/main.rs
@@ -4,6 +4,12 @@ use std::os::unix::io::AsRawFd;
 mod terminal;
 use terminal::Terminal;
 
+const fn ctrl_key(val: u8) -> u8 {
+    return val & 0b00011111;
+}
+
+const CTRL_Q: u8 = ctrl_key(b'q');
+
 fn is_cntrl(c: u8) -> bool {
     if c < 32 || c == 127 {
         return true;
@@ -11,19 +17,16 @@ fn is_cntrl(c: u8) -> bool {
     return false;
 }
 
-fn read_loop(handle: &mut StdinLock) {
+fn read_loop(handle: &mut StdinLock) -> io::Result<()> {
     let mut buf: [u8; 4];
 
     loop {
         buf = [0, 0, 0, 0];
 
-        match handle.read(&mut buf) {
-            Err(e) => println!("{}", e),
-            _ => {}
-        }
+        handle.read(&mut buf)?;
 
         match buf[0] {
-            b'q' => break,
+            CTRL_Q => break,
             c => {
                 if !is_cntrl(c) {
                     print!("{} ({})\r\n", c, c as char)
@@ -33,6 +36,8 @@ fn read_loop(handle: &mut StdinLock) {
             }
         }
     }
+
+    return Ok(());
 }
 
 fn main() -> io::Result<()> {
@@ -41,8 +46,8 @@ fn main() -> io::Result<()> {
     let terminal = Terminal::new(handle.as_raw_fd());
 
     terminal.enable_raw_mode();
-    read_loop(&mut handle);
+    read_loop(&mut handle)?;
     terminal.disable_raw_mode();
 
-    Ok(())
+    return Ok(());
 }
